6e6b1db5a9443a5848e926b0d1800d20f44d8a5f
James Schultz <ninjames101@gmail.com>
2019-02-09 17:10:07 -0700

    updated app with an app.get in order to have something display on the ngrok url 😎

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res, next) => {
+  res.send('we out here 🙏')
+})
+
 app.listen(port, () => {
   console.log(`got you on ${port}`)
 })
